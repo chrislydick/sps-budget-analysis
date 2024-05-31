898d3bf2932768d6a6506238d4ced910f025aa72
chrislydick <chris.lydick@gmail.com>
2024-05-30 19:25:16 -0700

    Update

--- app.py
+++ app.py
@@ -36,9 +36,6 @@ def move_column(dataframe, column, position):
     return dataframe[cols]
 
 
-
-
-
 # Load data
 data = pd.read_csv('data/performance_data_2023.csv')
 
@@ -64,9 +61,10 @@ data.drop(columns='Year', inplace=True)
 data = data.rename(columns={'Latitude': 'latitude', 'Longitude': 'longitude'})
 
 # Title
-st.title('Seattle Elementary Schools Budget Analysis')
-
-
+st.title('Simulation of School Closures in Seattle Public Schools')
+st.write('Seattle Public Schools (SPS) has initiated a program dubbed as <a href="https://www.seattleschools.org/resources/well-resourced-schools/">Well-Resourced Schools</a>, which was initiated upon board approval for analysis of up to 20 elementary schools to be closed in Seattle in order to address a growing budget gap in excess of $100M/year. This analysis utilizes <a href="https://github.com/chrislydick/sps-budget-analysis/tree/main/data">publicly available data</a> in order to understand outcomes of potential school closures. This data and analysis is provided for informational purposes only and is not intended to be a recommendation for or against any specific school closure. All code and data is available on <a href="https://github.com/chrislydick/sps-budget-analysis">GitHub here</a>.', unsafe_allow_html=True)
+st.write('')
+st.write('')
 
 
 
@@ -79,7 +77,7 @@ st.title('Seattle Elementary Schools Budget Analysis')
 
 
 # Sidebar for filtering
-st.sidebar.header('Adjust the filters below ')
+st.sidebar.header('Identify Schools to Close by Filters')
 
 
 selected_options =  st.sidebar.multiselect("Metrics to Find Schools to Close...",
@@ -93,13 +91,13 @@ color_options = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightre
 # Landmark status filter
 landmark_options = ['Y', 'N', 'P']
 if 'School Landmark Status' in selected_options:
-    selected_landmark = st.sidebar.multiselect('Select Landmark Status', options=landmark_options, default=landmark_options)
+    selected_landmark = st.sidebar.multiselect("School's Landmark Status:", options=landmark_options, default=landmark_options)
 else:
     selected_landmark = landmark_options
 
 # Budget range filter using sliders
 if 'School Budget' in selected_options:
-    budget_range = st.sidebar.slider('Select Total Budget Range', 
+    budget_range = st.sidebar.slider("School's Budget Range:", 
                                  min_value=0, 
                                  max_value=int(data['Total Budget (BUDGET)'].max()), 
                                  value=(0, int(data['Total Budget (BUDGET)'].max())),format='$%d')
@@ -108,7 +106,7 @@ else:
 
 # Excess Budget per Student filter using sliders
 if 'Excess Budget per Student' in selected_options:
-    excess_budget_range = st.sidebar.slider('Select Excess Budget per Student Range', 
+    excess_budget_range = st.sidebar.slider("School's Excess Budget per Student Range:", 
                                         min_value=float(data['Excess Budget per Student'].min()), 
                                         max_value=float(data['Excess Budget per Student'].max()), 
                                         value=(float(data['Excess Budget per Student'].min()), float(data['Excess Budget per Student'].max())),format='$%d')
@@ -117,7 +115,7 @@ else:
 
 # Budget Efficiency filter using sliders
 if 'Budget Efficiency' in selected_options:
-    budget_efficiency_range = st.sidebar.slider('Select Budget Efficiency Range', 
+    budget_efficiency_range = st.sidebar.slider("School's Budget per Student Range:", 
                                             min_value=float(data['Budget Efficiency'].min()), 
                                             max_value=float(data['Budget Efficiency'].max()), 
                                             value=(float(data['Budget Efficiency'].min()), float(data['Budget Efficiency'].max())),format='$%d')
@@ -127,7 +125,7 @@ else:
 
 # Disadvantage Score filter using sliders
 if 'Disadvantage Score' in selected_options:
-    disadvantage_score_range = st.sidebar.slider('Select Disadvantage Score Range', 
+    disadvantage_score_range = st.sidebar.slider("School's Disadvantage Score Range:", 
                                              min_value=0.0, 
                                              max_value=float(data['Disadvantage Score'].max()), 
                                              value=(0.0, float(data['Disadvantage Score'].max())))
@@ -137,7 +135,7 @@ else:
 
 # Distance to nearest school using sliders
 if 'Distance to Closest School' in selected_options:
-    distance_range = st.sidebar.slider('Select Distance to Closest School Range', 
+    distance_range = st.sidebar.slider("School's Distance to Closest School Range:", 
                                      min_value=0.0, 
                                      max_value=float(data['Distance to Closest School (miles)'].max()), 
                                      value=(0.0, float(data['Distance to Closest School (miles)'].max())))
@@ -147,7 +145,7 @@ else:
 
 # Total AAFTE Enrollment range filter using sliders
 if 'Enrollment Toal' in selected_options:
-    enrollment_range = st.sidebar.slider('Select Total AAFTE Enrollment Range', 
+    enrollment_range = st.sidebar.slider("School's total Enrollment Range:", 
                                      min_value=0, 
                                      max_value=int(data['Total AAFTE* Enrollment (ENROLLMENT)'].max()), 
                                      value=(0,300),format='%i')
@@ -157,7 +155,7 @@ else:
 
 # School Capacity range filter using sliders
 if 'Capacity Total' in selected_options:
-    capacity = st.sidebar.slider('Select Total School Capacity Percent Range', 
+    capacity = st.sidebar.slider("School's Capacity Percent Range:", 
                                      min_value=0.0, 
                                      max_value=float(data['Capacity Percent'].max()*100.0), 
                                      value=(0.0, 65.0), format='%i%%')
@@ -167,7 +165,7 @@ else:
 
 # Building Condition Score filter using sliders
 if 'Building Condition Score' in selected_options:
-    building_condition_score = st.sidebar.slider('Select Building Condition Score Range', 
+    building_condition_score = st.sidebar.slider("School's Building Condition Score Range:", 
                                      min_value=0.0, 
                                      max_value=float(data['Building Condition Score'].max()), 
                                      value=(0.0, float(data['Building Condition Score'].max())))
@@ -182,7 +180,7 @@ else:
     
 
 
-manual_school = st.sidebar.multiselect('Manually Select Additional Schools to Close', data['School'].unique())
+manual_school = st.sidebar.multiselect('Manually Select Additional Schools to Close:', data['School'].unique())
 
 
 # Apply filters
@@ -215,9 +213,9 @@ filtered_data = data[((data['Landmark'].isin(selected_landmark)) &
 # Main panel
 
 
+st.subheader('')
 
 col1, col2, col3, col4, col5 = st.columns(5)
-
 col1.metric('Updated Elementary Budget', value=f"${data['Total Budget (BUDGET)'].sum() - filtered_data['Total Budget (BUDGET)'].sum():,.0f}",delta=f"-{filtered_data['Total Budget (BUDGET)'].sum():,.0f}", delta_color="inverse")
 col2.metric('Schools Remaining Open', f"{len(data) - len(filtered_data)}", delta=f"-{len(filtered_data)}", delta_color="inverse")
 col3.metric('Total Student Enrollment', f"{data['Total AAFTE* Enrollment (ENROLLMENT)'].sum() - filtered_data['Total AAFTE* Enrollment (ENROLLMENT)'].sum():,.0f}", delta=f"-{filtered_data['Total AAFTE* Enrollment (ENROLLMENT)'].sum():,.0f}")
@@ -235,6 +233,11 @@ data_moved = move_column(data_moved, 'Capacity', 3)
 data_moved = move_column(data_moved, 'Capacity Percent', 4)
 data_moved['Capacity Percent'] = data_moved['Capacity Percent'].map(lambda x: f"{x:.0%}")
 data_moved.rename(columns={'Total AAFTE* Enrollment (ENROLLMENT)':'Enrollment'}, inplace=True)
+
+
+st.subheader('')
+
+st.write('Simulated School Closures List:')
 st.data_editor(data_moved, use_container_width=True, hide_index=True, width=10000) 
 
 # Map of school locations with different colors for filtered and non-filtered schools
@@ -266,14 +269,14 @@ try:
         color = closed_school_color if row['School'] in filtered_data['School'].values else determine_color(row['Capacity Percent'])
         folium.Marker(
             location=[row['latitude'], row['longitude']],
-            popup=row['School'],
+            popup=f"{row['School']} ({row['Capacity Percent']*100:.0f}%)",
             icon=folium.Icon(color=color)
         ).add_to(m)
     for _, row in data.iterrows():
         color = determine_color(row['Capacity Percent'])
         folium.Marker(
             location=[row['latitude'], row['longitude']],
-            popup=row['School'],
+            popup=f"{row['School']} ({row['Capacity Percent']*100:.0f}%)",
             icon=folium.Icon(color=color)
         ).add_to(n)
 except:
@@ -283,6 +286,7 @@ except:
 with col1a:
     try: 
         st.subheader('Capacity Before School Closure(s)')
+        st.write('Ligher Schools are of less capacity.')
         st_folium(n, width=700, height=500) 
     except:
         st.write("")
@@ -293,6 +297,7 @@ with col1a:
 with col2a: 
     try: 
         st.subheader('Capacity After School Closure(s)')
+        st.write('Red Schools have simulated to close.')
         st_folium(m, width=700, height=500) 
     except:
         st.write("")
@@ -322,7 +327,6 @@ ax.set_ylabel('Number of Schools')
 #ax.set_ylabel(y_dimension)
 #ax.legend()
 ###st.pyplot(fig)
-
 
 
 #st.subheader("All Data:")
